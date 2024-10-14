bc2c251d117f1144a3ac60eeb33898a4120c7efa
Pixelchino <kharchenko0990@yandex.ru>
2024-10-14 16:29:23 +0300

    Рабочий код

---
+++ meteora_settings.py
@@ -0,0 +1,10 @@
+meteora_btn_con = '//button[contains(@class, "bg-[#191C32]")]'
+jup_btn_con = '//*[@id="__next"]/div[2]/div[1]/div/div[4]/div[3]/div/button[2]/div/span[2]'
+DEFAULT_DELAY = 300
+page_url = 'https://app.meteora.ag/'
+jup_swap_url = 'https://jup.ag/'
+token_address = {'USDT' : 'Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB',
+                 'SOL' : 'So11111111111111111111111111111111111111112',
+                 'JLP' : '27G8MtK7VtTcCHkpASjSDdkWWYfoqT6ggEuKidVJidD4'}
+connect_button = '//html/body/div[2]/div[2]/div/div[3]/div/button[2]'
+transaction_button = '//html/body/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/button[2]'
