2f7244a247fc7e1a58e1ae10e8100ba115a7faff
Alexander <alex.smilex95@gmail.com>
2024-10-18 01:42:58 +0300

    Константы для софта Метеора

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 meteora_btn_con = '//button[contains(@class, "bg-[#191C32]")]'
 jup_btn_con = '//*[@id="__next"]/div[2]/div[1]/div/div[4]/div[3]/div/button[2]/div/span[2]'
 DEFAULT_DELAY = 300
-page_url = 'https://app.meteora.ag/'
+page_url = 'https://app.meteora.ag/' 
 jup_swap_url = 'https://jup.ag/'
 token_address = {'USDT' : 'Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB',
                  'SOL' : 'So11111111111111111111111111111111111111112',
